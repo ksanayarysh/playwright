5fb7f51a1a7c43bc43d553629a3bb51471cc3d99
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-01-05 17:42:37 -0300

    fix

--- tests/api/users.api.spec.js
+++ tests/api/users.api.spec.js
@@ -2,12 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('User API Tests', () => {
     test('GET user list', async ( { request } ) => {
-    const response = await request.get('https://jsonplaceholder.typicode.com/users');
+        const response = await request.get('https://jsonplaceholder.typicode.com/users');
 
-    expect(response.status()).toBe(200);
+        expect(response.status()).toBe(200);
 
-    const users = await response.json();
-    expect(users.length).toBeGreaterThan(0);
-    expect(users[0]).toHaveProperty('email');
+        const users = await response.json();
+        expect(users.length).toBeGreaterThan(0);
+        expect(users[0]).toHaveProperty('email');
     });
 });
